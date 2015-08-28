b419e80f526c6d390e2f703f86fe8908ec572d24
Alloffices <alloffices.nyc@gmail.com>
2015-08-28 13:32:13 -0400

    add username to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825021733) do
+ActiveRecord::Schema.define(version: 20150828172814) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -47,10 +47,12 @@ ActiveRecord::Schema.define(version: 20150825021733) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
