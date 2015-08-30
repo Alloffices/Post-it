05d7c07e645727f413e9d338800d9b5232cb4fab
Alloffices <alloffices.nyc@gmail.com>
2015-08-30 06:02:43 -0400

    stable 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :skill, :social_app, :email, :website, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :image) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :skill, :msg, :social_app, :website, :email, :password, :password_confirmation, :current_password, :image) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150829203410) do
+ActiveRecord::Schema.define(version: 20150830094232) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150829203410) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,6 +52,9 @@ ActiveRecord::Schema.define(version: 20150829203410) do
     t.boolean  "admin",                  default: false
     t.string   "skill"
     t.string   "msg"
+    t.string   "website"
+    t.string   "social_app"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
